5556e29ad2f18db73778ec7ddd2ae11c5e856f17
Michael Tiondi <michaeltiondi@gmail.om>
2025-06-15 14:50:45 +0200

    More Updates

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    public function index()
+    {
+        //return view('index');
+    }
+
+    public function show()
+    {
+        //return view('show');
+    }
 }
